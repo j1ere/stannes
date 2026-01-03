fdf7dd69525b2ae5d72cea495b8b453fe7beebef
j1ere <jeremywambua106@gmail.com>
2026-01-03 07:54:49 +0300

    sitemap date update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://stanneschaplaincy.com'
-  const currentDate = new Date("2025-01-01")
+  const currentDate = new Date()
 
   return [
     {
